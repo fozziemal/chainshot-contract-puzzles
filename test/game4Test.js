3df96dada45a971771e8122e6a1da14c88e2a669
fozziemal <fozziemal@gmail.com>
2022-05-25 18:57:07 -0700

    final solutions for all puzzles

--- test/game4Test.js
+++ test/game4Test.js
@@ -1,4 +1,5 @@
 const { assert } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("Game4", function() {
   it("should be a winner", async function() {
@@ -8,7 +9,11 @@ describe("Game4", function() {
 
     // nested mappings are rough :}
 
-    await game.win();
+    const signer = ethers.provider.getSigner(0);
+    const address = await signer.getAddress();
+    await game.connect(signer).write(address);
+    
+    await game.win(address);
 
     // leave this assertion as-is
     assert(await game.isWon(), "You did not win the game");
--- test/game5Test.js
+++ test/game5Test.js
@@ -8,7 +8,23 @@ describe("Game5", function() {
 
     // good luck
 
-    await game.win();
+    const threshold = 0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf;
+    
+    let signer = ethers.provider.getSigner(0);
+    
+    let wallet = new ethers.Wallet(ethers.Wallet.createRandom(), ethers.provider);
+    
+    while (wallet.address >= threshold) {
+      wallet = new ethers.Wallet(ethers.Wallet.createRandom(), ethers.provider);
+    }
+
+    // send some ETH to our newly created wallet!
+    await signer.sendTransaction({
+      to: wallet.address,
+      value: ethers.utils.parseUnits("1", "ether"),
+    });
+    
+    await game.connect(wallet).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), "You did not win the game");
